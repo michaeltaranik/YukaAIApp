9d6113b033fbd570954d20086cb7380044bdb4f4
michaeltaranik <taranikmichael32@gmail.com>
2024-07-23 13:56:10 +0200

    add ScanningView to scan QR's

--- YukaAI/MainTabViews/ScanningView.swift
+++ YukaAI/MainTabViews/ScanningView.swift
@@ -6,10 +6,19 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ScanningView: View {
+    @State private var isShowingScanner = true
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+    
+    
+    func handleScan(result: Result<ScanResult, ScanError>) {
+       isShowingScanner = false
+       // more code to come
     }
 }
 
