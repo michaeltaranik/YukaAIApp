836a47e31b5a12856ebbb6089a3221616162ad45
michaeltaranik <taranikmichael32@gmail.com>
2024-08-05 11:27:49 +0200

    add custom view and minor changes

---
+++ YukaAI/ToBeDeleted/TestView/GeometryEffect.swift
@@ -0,0 +1,18 @@
+//
+//  GeometryEffect.swift
+//  YukaAI
+//
+//  Created by Michael Taranik on 05.08.2024.
+//
+
+import SwiftUI
+
+struct GeometryEffect: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeometryEffect()
+}
